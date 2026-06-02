a12c4cb70d72f40627d8772581434f543848dd0d
Lioz Assa <35832172+liozassa@users.noreply.github.com>
2026-06-02 14:38:49 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// קובץ הגדרות - חיוך על חוט
+
+// 1. הקישור לקובץ ה-Google Sheets שלך (מופרד בפסיקים - csv)
+const sheetCsvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSyZh_yrrm043MWU-p2JXfS8iPbBzAfJDBWmO7C-ADPHam5nRd9J728FiDyxKuMhwdf7mPEDVMvdSNR/pub?gid=0&single=true&output=csv';
+
+// 2. הקישור לתמונת הלוגו
+const logoUrl = 'https://drive.google.com/thumbnail?id=1kyMX2_jqdaztDt-0eaXdG0rYNWyh6kxs&sz=w800';
+
+// 3. מספר הטלפון לוואטסאפ (קידומת 972, בלי אפס בהתחלה)
+const whatsappNumber = '972508114396';
