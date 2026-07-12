2834ff9eb1c0e2799a1fc4236a9b9b761ad09a66
Lioz Assa <35832172+liozassa@users.noreply.github.com>
2026-07-12 23:04:22 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -3,6 +3,10 @@
 // 1. הקישור לקובץ ה-Google Sheets שלך (מופרד בפסיקים - csv)
 const sheetCsvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSyZh_yrrm043MWU-p2JXfS8iPbBzAfJDBWmO7C-ADPHam5nRd9J728FiDyxKuMhwdf7mPEDVMvdSNR/pub?gid=0&single=true&output=csv';
 
+// מפתחות Supabase
+var supabaseUrl = 'https://rqegauvbnhxjuedypwus.supabase.co';
+var supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxZWdhdXZibmh4anVlZHlwd3VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4NzU5ODUsImV4cCI6MjA5OTQ1MTk4NX0.PYrmtCnGazOVJrPUYrmpihdOvxrjm70nLk0HJKaAbIA';
+
 // 2. הקישור לתמונת הלוגו
 const logoUrl = 'https://drive.google.com/thumbnail?id=1kyMX2_jqdaztDt-0eaXdG0rYNWyh6kxs&sz=w800';
 
